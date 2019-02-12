23f0ff60dbda5228159e9303bd006d03efdb3056
Cameron Fleming <nevexo@outlook.com>
2019-02-12 21:17:35 +0000

    Initial code for contact us form (incomplete)

--- js/contact.js
+++ js/contact.js
@@ -12,5 +12,37 @@ const message = urlParams.get('message');
 
 if (firstname != undefined) {
     // Only defined when there is data.
-
+    document.getElementById("contact-form").hidden = true // Remove contact form
+    let text = document.getElementById("form-thanks-message").innerHTML
+    dataSet = {
+        "first_name": firstname,
+        "last_name": lastname,
+        "email_address": email,
+        "message": message
+    }
+    // Find placeholders & replace them in the message
+    let inPlaceholder = false // Set to true if the cursor is in a placeholder
+    let placeholderString = ""
+    for (let i=0; i < text.length; i++) { // Increment the cursor position
+        if (text[i] == "]" && inPlaceholder) {
+            inPlaceholder = false
+            // Replace the placeholder
+            //text.replace(placeholderString,  "big peen")
+            text = text.replace("[" + placeholderString + "]", dataSet[placeholderString])
+            // Reset the placeholderString
+            placeholderString = ""
+            console.log("Exited placeholder")
+        }      
+        if (inPlaceholder) {
+            placeholderString += text[i]
+            console.log("Appending " + text[i])
+        }
+        if (text[i] == "[" && !inPlaceholder) {
+            inPlaceholder = true
+            console.log("Now in placeholder")
+        }
+    } 
+    // All done replacing placeholders, update the element
+    document.getElementById("form-thanks-message").innerHTML = text    
+    document.getElementById("form-thanks-message").hidden = false
 }
